0005779d6fe75da76b59e70e46cd3d9e92e551c1
Maxence Castel <maxence.nord@gmail.com>
2024-04-26 16:05:15 +0200

    add TypeMateriel model

---
+++ src/main/java/com/cashcash/TypeMateriel.java
@@ -0,0 +1,44 @@
+package com.cashcash;
+
+public class TypeMateriel {
+    
+    private String referenceInterne, libelleTypeMateriel;
+    private Famille laFamille;
+    
+    public TypeMateriel(String referenceInterne, String libelleTypeMateriel, Famille laFamille) {
+        this.referenceInterne = referenceInterne;
+        this.libelleTypeMateriel = libelleTypeMateriel;
+        this.laFamille = laFamille;
+    }
+
+    public String getReferenceInterne() {
+        return referenceInterne;
+    }
+
+    public void setReferenceInterne(String referenceInterne) {
+        this.referenceInterne = referenceInterne;
+    }
+
+    public String getLibelleTypeMateriel() {
+        return libelleTypeMateriel;
+    }
+
+    public void setLibelleTypeMateriel(String libelleTypeMateriel) {
+        this.libelleTypeMateriel = libelleTypeMateriel;
+    }
+
+    public Famille getLaFamille() {
+        return laFamille;
+    }
+
+    public void setLaFamille(Famille laFamille) {
+        this.laFamille = laFamille;
+    }
+
+    @Override
+    public String toString() {
+        return "TypeMateriel [referenceInterne=" + referenceInterne + ", libelleTypeMateriel=" + libelleTypeMateriel
+                + ", laFamille=" + laFamille + "]";
+    }
+
+}
