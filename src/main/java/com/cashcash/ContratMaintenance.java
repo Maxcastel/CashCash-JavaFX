12f034dac32816ef98284673ca2a1e801427c3e8
Maxence Castel <maxence.nord@gmail.com>
2024-04-26 16:07:12 +0200

    add ContratMaintenance model

---
+++ src/main/java/com/cashcash/ContratMaintenance.java
@@ -0,0 +1,73 @@
+package com.cashcash;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+
+public class ContratMaintenance {
+
+    private String numContrat;
+    private LocalDate dateSignature, dateEcheance;
+
+    private ArrayList<Materiel> lesMaterielsAssures;
+
+    public ContratMaintenance(String numContrat, LocalDate dateSignature, LocalDate dateEcheance,
+            ArrayList<Materiel> lesMaterielsAssures) {
+        this.numContrat = numContrat;
+        this.dateSignature = dateSignature;
+        this.dateEcheance = dateEcheance;
+        this.lesMaterielsAssures = new ArrayList<Materiel>();
+    }
+
+    public int getJoursRestants() {
+        LocalDate now = LocalDate.now();
+        long joursRestants = dateEcheance.until(now, ChronoUnit.DAYS);
+        return Math.toIntExact(joursRestants);
+    }
+
+    public boolean estValide() {
+        LocalDate now = LocalDate.now();
+        return now.isAfter(dateSignature) && now.isBefore(dateEcheance);
+    }
+
+    public void ajouteMateriel(Materiel unMateriel) {
+        if(dateSignature.isBefore(unMateriel.getDateInstallation())) {
+            lesMaterielsAssures.add(unMateriel);
+        }
+    }
+    
+
+    public String getNumContrat() {
+        return numContrat;
+    }
+
+    public void setNumContrat(String numContrat) {
+        this.numContrat = numContrat;
+    }
+
+    public LocalDate getDateSignature() {
+        return dateSignature;
+    }
+
+    public void setDateSignature(LocalDate dateSignature) {
+        this.dateSignature = dateSignature;
+    }
+
+    public LocalDate getDateEcheance() {
+        return dateEcheance;
+    }
+
+    public void setDateEcheance(LocalDate dateEcheance) {
+        this.dateEcheance = dateEcheance;
+    }
+
+    public ArrayList<Materiel> getLesMaterielsAssures() {
+        return lesMaterielsAssures;
+    }
+
+    public void setLesMaterielsAssures(ArrayList<Materiel> lesMaterielsAssures) {
+        this.lesMaterielsAssures = lesMaterielsAssures;
+    }
+    
+    
+}
