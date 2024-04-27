e2b9afa8c13f38f6e3312d5bb777d681ad9f22fe
Maxence Castel <maxence.nord@gmail.com>
2024-04-27 15:19:36 +0200

    PrimaryController: add ClientRepository and show each client NumClient with getAllClients()

--- src/main/java/com/cashcash/controllers/PrimaryController.java
+++ src/main/java/com/cashcash/controllers/PrimaryController.java
@@ -3,13 +3,21 @@ package com.cashcash.controllers;
 import java.io.IOException;
 
 import com.cashcash.App;
+import com.cashcash.entities.Client;
+import com.cashcash.repositories.ClientRepository;
 
 import javafx.fxml.FXML;
 
 public class PrimaryController {
 
+    private ClientRepository clientRepository = new ClientRepository();
+
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+
+        for (Client client : clientRepository.getAllClients()) {
+            System.out.println(client.getNumClient());
+        }
     }
 }
