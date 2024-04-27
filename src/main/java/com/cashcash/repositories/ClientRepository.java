1ffc4884e65ed7f59006c752c152157bd315bb30
Maxence Castel <maxence.nord@gmail.com>
2024-04-27 15:10:40 +0200

    ClientRepository: add getAllClients()

--- src/main/java/com/cashcash/repositories/ClientRepository.java
+++ src/main/java/com/cashcash/repositories/ClientRepository.java
@@ -1,7 +1,35 @@
 package com.cashcash.repositories;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.cashcash.Bdd;
+import com.cashcash.entities.Client;
+
 public class ClientRepository {
 
-    
+    public List<Client> getAllClients() {
+        List<Client> clients = new ArrayList<>();
+
+        try{
+            Bdd bdd = new Bdd();
+            
+            PreparedStatement ps = bdd.getConnection().prepareStatement("SELECT * FROM client");
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Client client = new Client();
+                client.setNumClient(rs.getInt("client_num"));
+                clients.add(client);
+            }
+        }
+        catch (Exception e) {
+			e.printStackTrace();	
+		}
+
+        return clients;
+    }
     
 }
