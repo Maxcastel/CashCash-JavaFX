823b6a2ebbcb60865d2ab59757b8136be8d0ed04
Maxence Castel <maxence.nord@gmail.com>
2024-05-09 23:18:58 +0200

    add ClientController

---
+++ src/main/java/com/cashcash/controllers/ClientController.java
@@ -0,0 +1,15 @@
+package com.cashcash.controllers;
+
+import javafx.event.ActionEvent;
+
+import javafx.fxml.FXML;
+
+public class ClientController {
+
+    @FXML
+    void OnClickRelanceClients(ActionEvent event) {
+        
+    }
+
+}
+
